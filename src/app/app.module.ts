771c44e3b8c757a5a1519404dad7155ad3e70e2a
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-10-21 22:17:36 +0300

    fix: пересоздает проект без роутов

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { HttpClientModule } from '@angular/common/http'
-import { StoreModule } from '@ngrx/store'
-import { coreReducer } from './core/state/reducers'
-import { StoreDevtoolsModule } from '@ngrx/store-devtools'
-import { EffectsModule } from '@ngrx/effects'
-import { CoreEffect } from './core/state/effects'
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    StoreModule.forRoot({
-      core: coreReducer
-    }),
-    StoreDevtoolsModule.instrument({
-      maxAge: 25
-    }),
-    EffectsModule.forRoot([ CoreEffect ])
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/state/actions.ts
+++
@@ -1,4 +0,0 @@
-import { createAction, props } from '@ngrx/store'
-
-export const initApp = createAction('[App] init app')
-export const storeDataAfterInitApp = createAction('[Model] Store data after init app', props<any>())
