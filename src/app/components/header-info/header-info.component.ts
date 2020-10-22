504de6d729f23e7b07f0c8f0adcd6896e1874e6c
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-10-22 22:26:52 +0300

    feat: добавлен вывод дат путешествия

--- src/app/components/header-info/header-info.component.ts
+++ src/app/components/header-info/header-info.component.ts
@@ -6,6 +6,8 @@ import { map, switchMap, take, tap } from 'rxjs/operators';
 import { IOffer, IPoint, IState } from 'src/app/models/models';
 import { selectPoints, selectRootState } from 'src/app/state/selectors';
 import { storeDataAfterInitApp } from 'src/app/state/actions';
+import { format } from 'date-fns';
+import { parseISO } from 'date-fns/fp';
 
 const MAXIMUM_CITIES_SHOWN = 3
 
@@ -29,6 +31,19 @@ export class HeaderInfoComponent {
       return this._getTripTitle(points)
     })
   )
+
+  public dates$: Observable<string> = this.points$.pipe(
+    map((points) => {
+      let tripDates = '';
+
+      if (points.length > 0) {
+        tripDates = this._getTripDatesTitle(points[0][`dateFrom`], points[points.length - 1][`dateTo`]);
+      }
+
+      return tripDates
+    })
+  )
+
   public cost$: Observable<number> = this.points$.pipe(
     map((points) => {
       return this._getTotalCost(points)
@@ -40,20 +55,28 @@ export class HeaderInfoComponent {
 
   private _getTotalCost(points: IPoint[]): number {
     const tripTotalCost = points.reduce((total, cost): number => {
-      const offersTotalPrice = this._countOffersTotalCost(cost.offers);
-      return total + cost.basePrice + offersTotalPrice;
-    }, 0);
-    return tripTotalCost;
+      const offersTotalPrice = this._countOffersTotalCost(cost.offers)
+      return total + cost.basePrice + offersTotalPrice
+    }, 0)
+    return tripTotalCost
   }
 
   private _countOffersTotalCost(offers: IOffer[]): number {
-    const offersTotalCost = offers ? offers.reduce((offersTotal, offerCost) => offersTotal + offerCost.price, 0) : 0;
-    return offersTotalCost;
+    const offersTotalCost = offers ? offers.reduce((offersTotal, offerCost) => offersTotal + offerCost.price, 0) : 0
+    return offersTotalCost
   }
 
   private _getTripTitle(points: IPoint[]): string {
-    const route = Array.from(new Set(points.map((point) => point.destination.name)));
+    const route = Array.from(new Set(points.map((point) => point.destination.name)))
 
-    return route.length <= MAXIMUM_CITIES_SHOWN ? route.join(' — ') : `${route.slice(0, 1)} — … — ${route.slice(route.length - 1)}`;
+    return route.length <= MAXIMUM_CITIES_SHOWN ? route.join(' — ') : `${route.slice(0, 1)} — … — ${route.slice(route.length - 1)}`
+  }
+
+  private _getTripDatesTitle(startDate: string, endDate: string): string {
+    if (format(parseISO(startDate), 'MMM') === format(parseISO(endDate), 'MMM')) {
+      return `${format(parseISO(startDate), 'MMM d')} — ${format(parseISO(endDate), 'd')}`
+    }
+
+    return `${format(parseISO(startDate), 'MMM d')} — ${format(parseISO(endDate), 'MMM d')}`
   }
 }
